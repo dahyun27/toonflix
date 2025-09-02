5eecb96d94c1b8b6a6640d06d8f9532d33b3136d
하다현 <dahyun@hadahyeons-MacBook-Pro.local>
2025-09-02 13:29:04 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,35 +10,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFFE7626C),
-        textTheme: TextTheme(
-          headlineSmall: TextStyle(color: Color(0xFF232B55)),
-        ),
-        cardColor: Color(0xFFF4EDDB),
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({super.key});
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "My Large title",
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
-    );
+    return MaterialApp(home: HomeScreen());
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,116 +1,19 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
-  bool isRunning = false;
-
-  late Timer? timer;
-
-  void onTick(timer) {
-    setState(() {
-      totalSeconds -= 1;
-    });
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(Duration(seconds: 1), onTick);
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausePressed() {
-    timer!.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Column(
-        children: [
-          Flexible(
-            fit: FlexFit.tight,
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                '$totalSeconds',
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 89,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            flex: 3,
-            child: Container(
-              child: IconButton(
-                iconSize: 120,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: isRunning
-                    ? Icon(Icons.pause_circle_outline)
-                    : Icon(Icons.play_circle_outline),
-              ),
-            ),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Pomodors",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(
-                              context,
-                            ).textTheme.headlineSmall!.color,
-                          ),
-                        ),
-                        Text(
-                          '0',
-                          style: TextStyle(
-                            fontSize: 58,
-                            fontWeight: FontWeight.w600,
-                            color: Theme.of(
-                              context,
-                            ).textTheme.headlineSmall!.color,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        elevation: 4,
+        title: Text(
+          "Today's Webtoon",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
       ),
     );
   }
